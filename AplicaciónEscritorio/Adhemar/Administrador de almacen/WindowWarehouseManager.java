dc62d488c3e45d5b1413de105c81a6a03bce3bc5
Adhemar03 <55875871+Adhemar03@users.noreply.github.com>
2019-10-04 03:54:28 -0400

    ac

--- AplicaciónEscritorio/Adhemar/Administrador de almacen/WindowWarehouseManager.java
+++ AplicaciónEscritorio/Adhemar/Administrador de almacen/WindowWarehouseManager.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package regprod;
+package interfaz;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
