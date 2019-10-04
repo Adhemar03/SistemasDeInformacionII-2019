d7b52fa417e0240cb044c8e394bab9b61f0e5362
Adhemar03 <55875871+Adhemar03@users.noreply.github.com>
2019-10-04 00:51:32 -0400

    Create WindowWarehouseManager.java

---
+++ AplicaciónEscritorio/Adhemar/Administrador de almacen/WindowWarehouseManager.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regprod;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+/**
+ *
+ * @author Adhemar
+ */
+public class WindowWarehouseManager extends JFrame implements ActionListener{
+    JButton registrar=new JButton("Registrar Medicamento");
+    public WindowWarehouseManager()
+    {
+        this.setLayout(null);
+        this.setTitle("Administrador de Almacen");
+        this.setSize(1200, 900);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        registrar.setBounds(50, 80, 180, 40);
+        registrar.addActionListener(this);
+        add(registrar);
+        
+        //crearFormulario();
+        this.setVisible(true);
+    }
+    public void openF()
+    {
+        new Formulario();
+    }
+    
+        public static void main(String[] args) {
+        new WindowWarehouseManager();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==registrar)
+        {
+            openF();
+        }
+    }
+}
